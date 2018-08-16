866440ad24d0886ceb6720dd8e2529a67285f794
Brandan Quinn <Brandan.quinn@isobar.com>
2018-08-16 10:33:17 -0400

    Initial commit

---
+++ fashion-test.py
@@ -0,0 +1,95 @@
+import tensorflow as tf
+from tensorflow import keras
+
+import numpy as np
+import matplotlib as mpl
+mpl.use('TkAgg')
+import matplotlib.pyplot as plt
+print(tf.__version__)
+
+fashion_mnist = keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+train_images = train_images / 255.0
+
+test_images = test_images / 255.0
+
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid('off')
+    plt.imshow(train_images[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[train_labels[i]])
+
+plt.show()    
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation=tf.nn.relu),
+    keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+
+model.compile(optimizer=tf.train.AdamOptimizer(), 
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=5)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+print('Test accuracy:', test_acc)
+
+predictions = model.predict(test_images)
+
+# Plot the first 25 test images, their predicted label, and the true label
+# Color correct predictions in green, incorrect predictions in red
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid('off')
+    plt.imshow(test_images[i], cmap=plt.cm.binary)
+    predicted_label = np.argmax(predictions[i])
+    true_label = test_labels[i]
+    if predicted_label == true_label:
+      color = 'green'
+    else:
+      color = 'red'
+    plt.xlabel("{} ({})".format(class_names[predicted_label], 
+                                  class_names[true_label]),
+                                  color=color)
+plt.show()
+
+# continuously test input in trained model
+# have user input number, run that number test_image through model
+# get prediction, display image
+
+user_input = 0
+
+while True:
+    user_input = int(input('Enter a number between 0 and 10000: '))
+    if user_input < 0 or user_input >= 10000: 
+        print('Input out of bounds.')
+        break
+    img = test_images[user_input]
+    img = (np.expand_dims(img, 0))
+
+    predictions = model.predict(img)
+    print('Test image predicted value: ', np.argmax(predictions[0]))
+
+    plt.figure()
+    plt.imshow(test_images[user_input])
+    predicted_label = np.argmax(predictions[0])
+    true_label = test_labels[user_input]
+    if predicted_label == true_label:
+        color = 'green'
+    else:
+        color = 'red'
+    plt.xlabel("{} ({})".format(class_names[predicted_label], 
+                                  class_names[true_label]),
+                                  color=color)
+    plt.show()
