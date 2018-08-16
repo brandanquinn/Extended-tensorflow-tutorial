d6b3bb795bee049a96d4838ec1d89759bbe370b2
Brandan Quinn <Brandan.quinn@isobar.com>
2018-08-16 16:47:29 -0400

    Initial commit

---
+++ text-classification.py
@@ -0,0 +1,93 @@
+import tensorflow as tf
+from tensorflow import keras
+
+import numpy as np
+
+print(tf.__version__)
+
+imdb = keras.datasets.imdb
+
+(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
+
+# print(train_data[0])
+
+# A dictionary mapping words to an integer index
+word_index = imdb.get_word_index()
+
+# The first indices are reserved
+word_index = {k:(v+3) for k,v in word_index.items()} 
+word_index["<PAD>"] = 0
+word_index["<START>"] = 1
+word_index["<UNK>"] = 2  # unknown
+word_index["<UNUSED>"] = 3
+
+reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
+
+def decode_review(text):
+    return ' '.join([reverse_word_index.get(i, '?') for i in text])
+
+def encode_review(text):
+    myList = []
+    for word in text.split():
+        if word in word_index:
+            myList.append(word_index[word])
+    return myList
+
+# print(decode_review(train_data[0]))
+
+train_data = keras.preprocessing.sequence.pad_sequences(train_data,
+                                                        value=word_index["<PAD>"],
+                                                        padding='post',
+                                                        maxlen=256)
+
+test_data = keras.preprocessing.sequence.pad_sequences(test_data,
+                                                       value=word_index["<PAD>"],
+                                                       padding='post',
+                                                       maxlen=256)
+
+# input shape is the vocabulary count used for the movie reviews (10,000 words)
+vocab_size = 10000
+
+model = keras.Sequential()
+model.add(keras.layers.Embedding(vocab_size, 16))
+model.add(keras.layers.GlobalAveragePooling1D())
+model.add(keras.layers.Dense(16, activation=tf.nn.relu))
+model.add(keras.layers.Dense(1, activation=tf.nn.sigmoid))
+
+model.summary()
+
+model.compile(optimizer=tf.train.AdamOptimizer(),
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+x_val = train_data[:10000]
+partial_x_train = train_data[10000:]
+
+y_val = train_labels[:10000]
+partial_y_train = train_labels[10000:]
+
+history = model.fit(partial_x_train,
+                    partial_y_train,
+                    epochs=40,
+                    batch_size=512,
+                    validation_data=(x_val, y_val),
+                    verbose=1)
+
+results = model.evaluate(test_data, test_labels)
+
+print(results)
+
+my_review = 'This movie was bad I hated every second of it '
+encoded_review = encode_review(my_review.lower())
+print(decode_review(encoded_review))
+encoded_review = (np.expand_dims(encoded_review, 0))
+predictions = model.predict(encoded_review)
+print('Predicted value for custom review: ', int(round(predictions[0][0])))
+
+review = train_data[0]
+print(decode_review(review))
+review = (np.expand_dims(review, 0))
+
+predictions = model.predict(review)
+print('Predicted value for test_data: ', int(round(predictions[0][0])))
+print('Actual value for test_data: ', train_labels[0])
