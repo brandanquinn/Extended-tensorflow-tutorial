eb5ff7c48cd9634034d588fb767cda770284473d
Brandan Quinn <Brandan.quinn@isobar.com>
2018-08-17 13:42:02 -0400

    Added extra comments to explain each section of the code.

--- simple-classification.py
+++ simple-classification.py
@@ -2,20 +2,25 @@ import tensorflow as tf
 from tensorflow import keras
 
 import numpy as np
+# Needed to set a mpl backend variable
 import matplotlib as mpl
 mpl.use('TkAgg')
 import matplotlib.pyplot as plt
 print(tf.__version__)
 
+# Load built-in image dataset.
 fashion_mnist = keras.datasets.fashion_mnist
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
+# List of classifications
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
-train_images = train_images / 255.0
 
+# Preprocessing of the data done in order to scale the pixel values into floats within the range of 0 to 1.
+train_images = train_images / 255.0
 test_images = test_images / 255.0
 
+# Visualize examples of images within the dataset.
 plt.figure(figsize=(10,10))
 for i in range(25):
     plt.subplot(5,5,i+1)
@@ -27,21 +32,26 @@ for i in range(25):
 
 plt.show()    
 
+# Initialize the model with Dense layers and Flattens format of images from 2d (28x28) to 1d array of 784 pixels.
 model = keras.Sequential([
     keras.layers.Flatten(input_shape=(28, 28)),
     keras.layers.Dense(128, activation=tf.nn.relu),
     keras.layers.Dense(10, activation=tf.nn.softmax)
 ])
 
+# Go-to optimizer, sparse_categorical_crossentropy used since we have several possible classifications.
 model.compile(optimizer=tf.train.AdamOptimizer(), 
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
+# Train the model.
 model.fit(train_images, train_labels, epochs=5)
 
+# Compute the accuracy of the trained model using the sliced test set
 test_loss, test_acc = model.evaluate(test_images, test_labels)
 print('Test accuracy:', test_acc)
 
+# Gets predictions for test images to display in visualization below.
 predictions = model.predict(test_images)
 
 # Plot the first 25 test images, their predicted label, and the true label
--- text-classification.py
+++ text-classification.py
@@ -7,11 +7,9 @@ import string
 
 print(tf.__version__)
 
+# Loading built-in IMDB review dataset
 imdb = keras.datasets.imdb
-
 (train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
-
-# print(train_data[0])
 
 # A dictionary mapping words to an integer index
 word_index = imdb.get_word_index()
@@ -25,9 +23,14 @@ word_index["<UNUSED>"] = 3
 
 reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
 
+# In order to process the reviews through the model, they must be encoded to unique integers. 
+# Since the dataset is handled by tensorflow internally - it starts as the encoded list.
 def decode_review(text):
     return ' '.join([reverse_word_index.get(i, '?') for i in text])
 
+# Because I wanted to have the option to process custom reviews, I needed to develop 
+# function to encode the review based on the pre-existing data. This is done by converting all words to lowercase, removing
+# punctuation and then mapping each cleaned word through provided dictionary(python map) to get integer value.
 def encode_review(text):
     text = text.lower()
     translator = str.maketrans('', '', string.punctuation)
@@ -53,24 +56,30 @@ test_data = keras.preprocessing.sequence.pad_sequences(test_data,
 # input shape is the vocabulary count used for the movie reviews (10,000 words)
 vocab_size = 10000
 
+# set up model
 model = keras.Sequential()
 model.add(keras.layers.Embedding(vocab_size, 16))
 model.add(keras.layers.GlobalAveragePooling1D())
 model.add(keras.layers.Dense(16, activation=tf.nn.relu))
 model.add(keras.layers.Dense(1, activation=tf.nn.sigmoid))
 
+# print model information to console
 model.summary()
 
+# Loss function and optimizer set for training purposes. Since we are classifying in
+# a binary manor (positive/negative) we use the binary_crossentropy function for loss.
 model.compile(optimizer=tf.train.AdamOptimizer(),
               loss='binary_crossentropy',
               metrics=['accuracy'])
 
+# Slicing the dataset to get training and testing values.
 x_val = train_data[:10000]
 partial_x_train = train_data[10000:]
 
 y_val = train_labels[:10000]
 partial_y_train = train_labels[10000:]
 
+# Train the model based on the sliced training values. Ran over 40 epochs with batch size of 512.
 history = model.fit(partial_x_train,
                     partial_y_train,
                     epochs=40,
@@ -78,11 +87,11 @@ history = model.fit(partial_x_train,
                     validation_data=(x_val, y_val),
                     verbose=1)
 
+# Evaluate the trained model with testing data and compute result.
 results = model.evaluate(test_data, test_labels)
-
 print(results)
-my_review = ''
 
+# Hold program with trained model in order to type custom reviews and use the model to predict whether they are positive or negative.
 while True:
     my_review = input('Write a movie review (type done when you are finished): ')
     if my_review.lower() == 'done': break
